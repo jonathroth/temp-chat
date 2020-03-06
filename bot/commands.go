2cd7d3a318c54439370bb080fc41b55fddffc1b0
Jonathan Roth <jonathroth@gmail.com>
2020-03-06 20:58:25 +0200

    Added a "setup required" condition, added help command

--- bot/commands.go
+++ bot/commands.go
@@ -14,11 +14,13 @@ const (
 )
 
 // Commands maps between the command name and the command logic.
-var Commands = map[string]Command{
-	makeChannelCommand: Command{AdminOnly: false},
-	"set-mkch":         Command{AdminOnly: true},
-	"set-command-ch":   Command{AdminOnly: true},
-	"setup":            Command{AdminOnly: true},
+var Commands = map[string]*Command{
+	makeChannelCommand: &Command{SetupRequired: true, AdminOnly: false},
+	"set-mkch":         &Command{SetupRequired: true, AdminOnly: true},
+	"set-prefix":       &Command{SetupRequired: true, AdminOnly: true},
+	"set-command-ch":   &Command{SetupRequired: true, AdminOnly: true},
+	"setup":            &Command{SetupRequired: false, AdminOnly: true},
+	"help":             &Command{SetupRequired: false, AdminOnly: false, Handler: helpHandler},
 }
 
 // CommandHandler is a handler func called when a command is successfully parsed.
@@ -27,8 +29,10 @@ type CommandHandler func(s *discordgo.Session, m *discordgo.MessageCreate, args 
 
 // Command defines the logic and conditions required for a command to run.
 type Command struct {
-	AdminOnly bool
-	Handler   CommandHandler
+	// SetupRequired tells whether the command can be run before the bot was configured for the server.
+	SetupRequired bool
+	AdminOnly     bool
+	Handler       CommandHandler
 }
 
 // MessageCreate is called whenever a message arrives in a server the bot is in.
@@ -38,15 +42,10 @@ func (b *TempChannelBot) MessageCreate(s *discordgo.Session, m *discordgo.Messag
 		log.Fatalf("Failed to parse discord ID: %v", err)
 	}
 
-	serverData, found := b.servers[serverID]
-	if !found {
-		// The server owner still hasn't called the setup command
-		// Avoid logging/sending a message to not flood the logs/servers
-		return
-	}
+	serverData, serverIsSetup := b.servers[serverID]
 
 	prefix := defaultPrefix
-	if serverData.CommandPrefix != "" {
+	if serverIsSetup && serverData.CommandPrefix != "" {
 		prefix = serverData.CommandPrefix
 	}
 
@@ -58,13 +57,18 @@ func (b *TempChannelBot) MessageCreate(s *discordgo.Session, m *discordgo.Messag
 	commandText := strings.TrimPrefix(m.Content, prefix)
 	commandParts := strings.Split(commandText, " ")
 
-	if serverData.CustomCommand != "" && commandParts[0] == serverData.CustomCommand {
+	if serverIsSetup && serverData.CustomCommand != "" && commandParts[0] == serverData.CustomCommand {
 		commandParts[0] = makeChannelCommand
 	}
 
 	command, found := Commands[commandParts[0]]
 	if !found {
-		b.replyToSenderAndLog(s, m.ChannelID, "Unknown command %v", commandParts[0])
+		b.replyToSenderAndLog(s, m.ChannelID, "Unknown command %q", commandParts[0])
+		return
+	}
+
+	if command.SetupRequired && !serverIsSetup {
+		b.replyToSenderAndLog(s, m.ChannelID, "The bot hasn't been set up yet, please use %vsetup first", prefix)
 		return
 	}
 
@@ -83,4 +87,31 @@ func (b *TempChannelBot) replyToSenderAndLog(s *discordgo.Session, channelID str
 	if err != nil {
 		log.Fatalf("Failed sending message response: %v", err)
 	}
+}
+
+func helpHandler(s *discordgo.Session, m *discordgo.MessageCreate, args []string) error {
+	_, err := s.ChannelMessageSend(m.ChannelID, "```"+`css
+[TempChat]
+TempChat is a bot that creates temporary text channels for Discord voice chats.
+
+The bot will give permission to any new user that joins the voice chat, and revoke the permission to any user that leaves it.
+
+#Commands:
+[Before Setup]
+!help - Displays this menu
+!setup [category-id] - Sets up the bot
+
+[After Setup]
+!mkch - Creates a temporary voice channel for the users in your voice chat
+!set-prefix [new-prefix] - Changes the command prefix
+[?]set-prefix - Resets the command prefix to !
+!set-mkch [new-name] - Changes the !mkch command to the desired command name
+!set-mkch - Resets the command name to !mkch
+!set-command-ch [channel-name] - Sets a specific channel for the bot to read commands from, the bot will ignore all other channels.
+!set-command-ch - Removes the specified command channel`+"```")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
